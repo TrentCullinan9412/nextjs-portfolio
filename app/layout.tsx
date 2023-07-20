436bded01820df485843f52a1aac1ba964f2932b
TrentCullinan9412 <trent.cullinan9412@gmail.com>
2023-07-20 12:00:11 -0500

    Change user button afterSignOutUrl to landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
                   </Button>
                 </SignedOut>
                 <SignedIn>
-                  <UserButton />
+                  <UserButton afterSignOutUrl="/" />
                 </SignedIn>
               </div>
             </div>
