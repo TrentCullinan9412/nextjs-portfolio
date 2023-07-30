8d154c8e3559537694ac9edc8f6fc98755f76fc3
TrentCullinan9412 <trent.cullinan9412@gmail.com>
2023-07-29 21:03:48 -0500

    Add DndProvider with HTML5Backend to email-editor

--- components/email-editor/index.tsx
+++ components/email-editor/index.tsx
@@ -1,23 +1,29 @@
+"use client";
+
 import { Canvas } from "./canvas";
 import { ComponentMenu } from "./component-menu";
 import { PropertyEditor } from "./property-editor";
 import { EmailContextProvider } from "./context/provider";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import { ExampleOne } from "./examples/example-one";
 
 export default function EmailEditor() {
   return (
     <div className="grid grid-cols-10 w-full h-[90vh] gap-4">
-      <ComponentMenu />
+      <DndProvider backend={HTML5Backend}>
+        <ComponentMenu />
 
-      <EmailContextProvider tree={ExampleOne}>
-        <div className="col-span-7">
-          <Canvas />
-        </div>
-        <div className="col-span-2">
-          <PropertyEditor />
-        </div>
-      </EmailContextProvider>
+        <EmailContextProvider tree={ExampleOne}>
+          <div className="col-span-7">
+            <Canvas />
+          </div>
+          <div className="col-span-2">
+            <PropertyEditor />
+          </div>
+        </EmailContextProvider>
+      </DndProvider>
     </div>
   );
 }
