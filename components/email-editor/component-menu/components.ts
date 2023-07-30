5a854c99d5e8f85d6f05854cdd1f287f2401d3df
TrentCullinan9412 <trent.cullinan9412@gmail.com>
2023-07-29 21:03:28 -0500

    Move component-menu to folder

--- components/email-editor/component-menu/components.ts
+++ components/email-editor/component-menu/components.ts
@@ -10,65 +10,66 @@ import {
   TableIcon,
   TextIcon,
 } from "@radix-ui/react-icons";
-import { Label } from "../ui/label";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { NodeType } from "../types";
+import { IconProps } from "@radix-ui/react-icons/dist/types";
 
-const components = [
+export type Component = {
+  name: string;
+  nodeType: NodeType;
+  Icon: React.ForwardRefExoticComponent<
+    IconProps & React.RefAttributes<SVGSVGElement>
+  >;
+};
+
+export const Components: Component[] = [
   {
     name: "Container",
+    nodeType: "container",
     Icon: GridIcon,
   },
   {
     name: "Column",
+    nodeType: "column",
     Icon: ColumnsIcon,
   },
   {
     name: "Row",
+    nodeType: "row",
     Icon: RowsIcon,
   },
   {
     name: "Section",
+    nodeType: "section",
     Icon: TableIcon,
   },
   {
     name: "Button",
+    nodeType: "button",
     Icon: ButtonIcon,
   },
   {
     name: "Link",
+    nodeType: "anchor",
     Icon: Link2Icon,
   },
   {
     name: "Image",
+    nodeType: "image",
     Icon: ImageIcon,
   },
   {
     name: "Horizontal Row",
+    nodeType: "hr",
     Icon: DividerHorizontalIcon,
   },
   {
     name: "Heading",
+    nodeType: "heading",
     Icon: HeadingIcon,
   },
   {
     name: "Paragraph",
+    nodeType: "paragraph",
     Icon: TextIcon,
   },
 ];
-
-export function ComponentMenu() {
-  return (
-    <RadioGroup className="flex flex-col gap-4">
-      {components.map(({ name, Icon }) => (
-        <Label
-          key={name}
-          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
-        >
-          <RadioGroupItem id={name} value={name} className="sr-only" />
-          <Icon className="mb-2 h-6 w-6" />
-          {name}
-        </Label>
-      ))}
-    </RadioGroup>
-  );
-}
---
+++ components/email-editor/component-menu/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Label } from "../../ui/label";
+import { RadioGroup, RadioGroupItem } from "../../ui/radio-group";
+import { useDrag } from "react-dnd";
+import { Component, Components } from "./components";
+
+function DraggableComponent({ name, nodeType, Icon }: Component) {
+  const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
+    type: nodeType,
+    item: { nodeType },
+    options: {
+      dropEffect: "copy",
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
+  if (isDragging) {
+    return <p ref={dragPreview}>Hello!</p>;
+  }
+
+  return (
+    <Label
+      ref={drag}
+      className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+    >
+      <RadioGroupItem id={name} value={name} className="sr-only" />
+      <Icon className="mb-2 h-6 w-6" />
+      {name}
+    </Label>
+  );
+}
+
+export function ComponentMenu() {
+  return (
+    <RadioGroup className="flex flex-col gap-4">
+      {Components.map((props) => (
+        <DraggableComponent key={props.name} {...props} />
+      ))}
+    </RadioGroup>
+  );
+}
